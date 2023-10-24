38d5979a03c114efb5d395ae35ed48420447bcf4
KoodoShujin <flo4biz@gmail.com>
2023-10-24 14:39:27 +0100

    setup email api

---
+++ index.server.js
@@ -0,0 +1,21 @@
+// server.js
+const express = require("express");
+const cors = require("cors");
+const env = require("dotenv");
+
+const emailApi = require("./src/api/email");
+
+//configure env
+env.config();
+
+//instantiating the server
+const app = express();
+
+//bodyParser/expressjs for passing datas
+app.use(cors());
+app.use(express.json());
+app.use("/api/email", emailApi);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
