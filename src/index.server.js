0292935a168596dcaa0c9ed5cd8b4948e59c36dc
KoodoShujin <flo4biz@gmail.com>
2023-11-01 23:33:28 +0100

    _-_

--- src/index.server.js
+++ src/index.server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const env = require("dotenv");
 
 const emailApi = require("./api/email");
+const initApi = require("./api/init");
 
 //configure env
 env.config();
@@ -15,6 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/email", emailApi);
+app.use("/api/init", initApi);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
